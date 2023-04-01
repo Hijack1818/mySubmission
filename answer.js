d1db6d74e0a44e9b404a3f2ea27b4c405a2af95b
Hijack1818 <100959224+Hijack1818@users.noreply.github.com>
2023-04-01 15:44:06 +0530

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,21 @@
+function fizz(number){
+    
+    var sum=0;
+    while(number){
+        sum += parseInt(number%10);
+        number = parseInt(number/10);
+    }
+    var ans =""
+    for(var i=1;i<=sum;i++){
+        if(i%3==0 && i%4==0)ans += "FizzBuzz ";
+        
+        else if(i%3==0)ans += "Fizz ";
+        
+        else if(i%4==0)ans += "Buzz ";
+        
+        else ans += `${i}`+" ";
+    }
+    console.log(ans);
+    
+}
+fizz(7009035854)
